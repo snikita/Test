15043bbd37362ed37e065d7896a451badf822f7a
Nikita <nikita@nikita-ThinkPad-X220.(none)>
2012-10-19 20:55:38 +0400

    test

---
+++ app/view/Login.js
@@ -0,0 +1,20 @@
+Ext.define("RateMyCat.view.Login", {
+    extend: 'Ext.Img',
+
+    config: {
+        title: null,
+        description: null
+    },
+
+    //sets up our tap event listener
+    initialize: function() {
+        this.callParent(arguments);
+
+        this.element.on('tap', this.onTap, this);
+    },
+
+    //this is called whenever you tap on the image
+    onTap: function() {
+        Ext.Msg.alert(this.getTitle(), this.getDescription());
+    }
+});
